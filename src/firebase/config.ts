bf8411da3f76fd8032598046f67cb06a4a69916c
Surya G Raghavan <Surya.Raghvan@gmail.com>
2025-10-21 20:35:24 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-639775235-cd249",
+  "appId": "1:592424759793:web:608591efdd65a410cb9450",
+  "apiKey": "AIzaSyCap0VvBzLOiu_Ya6jedkhqPjcxezTQlNo",
+  "authDomain": "studio-639775235-cd249.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "592424759793"
+};
