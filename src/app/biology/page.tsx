9eea6934c6a41692af10693f68db00d853da2438
Surya Raghavan <surya.raghvan@gmail.com>
2025-11-30 18:41:06 -0500

    added cell energy

--- src/app/biology/page.tsx
+++ src/app/biology/page.tsx
@@ -20,36 +20,43 @@ const notes = [
     topic: "CHARACTERISTICS OF LIVING THINGS",
     notesUrl: "https://drinks-hunt-3eb.craft.me/asdasdasdasdas",
     pdfUrl:
-      "https://drive.google.com/drive/folders/1Xdo1VFXf9z6dhP_b2iU_Om4dImFpqbu6?usp=sharing",
+      "https://drive.google.com/drive/folders/1Xdo1VFXf9z6dhP_b2iU_Om4dImFpqbu6",
     quizletUrl: "",
   },
   {
     topic: "ORGANIC MOLECULES",
     notesUrl: "https://drinks-hunt-3eb.craft.me/asfdsafadsfdsfs",
     pdfUrl:
-      "https://drive.google.com/drive/folders/160OPAQYfAuRNVn-jtAW35uyzMExr93UR?usp=sharing",
+      "https://drive.google.com/drive/folders/160OPAQYfAuRNVn-jtAW35uyzMExr93UR",
     quizletUrl: "",
   },
   {
     topic: "ENZYMES",
     notesUrl: "https://drinks-hunt-3eb.craft.me/fdgdfgdfgdfgdfgdf",
     pdfUrl:
-      "https://drive.google.com/drive/folders/1P6jHR7n_gQww9U2vr5ZJXlIPeMrjXc4r?usp=sharing",
+      "https://drive.google.com/drive/folders/1P6jHR7n_gQww9U2vr5ZJXlIPeMrjXc4r",
     quizletUrl: "",
   },
   {
     topic: "THE CELL",
     notesUrl: "https://drinks-hunt-3eb.craft.me/BWwKgb2BT5sgbZ",
     pdfUrl:
-      "https://drive.google.com/drive/folders/10BOS_ghIpDbgTZaoRsImsM8_0qWBruVo?usp=sharing",
+      "https://drive.google.com/drive/folders/10BOS_ghIpDbgTZaoRsImsM8_0qWBruVo",
     quizletUrl: "",
   },
   {
     topic: "CELLULAR TRANSPORT",
     notesUrl: "https://drinks-hunt-3eb.craft.me/FChuDvbuWVFkSs",
     pdfUrl:
-      "https://drive.google.com/drive/folders/1y4IBlejw7VUZzQNb6IaN8UY4dwgA9WxJ?usp=sharing",
+      "https://drive.google.com/drive/folders/1y4IBlejw7VUZzQNb6IaN8UY4dwgA9WxJ",
     quizletUrl: "https://quizlet.com/1111521452/cellular-transport-flash-cards/",
+  },
+  {
+    topic: "CELL ENERGY",
+    notesUrl: "https://drinks-hunt-3eb.craft.me/yE51rTLi4yFyDq",
+    pdfUrl:
+      "https://drive.google.com/drive/folders/1OFYuGjfqj8PzCHXg7T-COx3qrT5xGaDx",
+    quizletUrl: "",
   },
 ];
 
