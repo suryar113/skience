cee6a9a383a8d1f09036744362827dde2992823c
Surya Raghavan <Surya.Raghvan@gmail.com>
2025-12-19 03:35:49 +0000

    cursor turns into clickable cursor, but the when clicked, the link doesn

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -144,20 +144,35 @@ export function SiteHeader() {
         >
           {navItems.map((item, index) => (
             <Tab key={item.href} setPosition={setPosition}>
-              <Link href={item.href} ref={el => linkRefs.current[index] = el ? el.parentElement : null} className="w-full h-full flex items-center justify-center">{item.label}</Link>
+              <Link href={item.href} ref={el => {
+                if (el) {
+                  const parentLi = el.parentElement as HTMLLIElement;
+                  linkRefs.current[index] = parentLi;
+                }
+              }} className="w-full h-full flex items-center justify-center">{item.label}</Link>
             </Tab>
           ))}
           <li className='nav-pill-separator'></li>
           {actionItems.map((item, index) => (
              <Tab key={item.label} setPosition={setPosition}>
-                <Link href={item.href} target="_blank" rel="noopener noreferrer" ref={el => linkRefs.current[navItems.length + 1 + index] = el ? el.parentElement : null} className="w-full h-full flex items-center justify-center">
+                <Link href={item.href} target="_blank" rel="noopener noreferrer" ref={el => {
+                  if (el) {
+                    const parentLi = el.parentElement as HTMLLIElement;
+                    linkRefs.current[navItems.length + 1 + index] = parentLi;
+                  }
+                }} className="w-full h-full flex items-center justify-center">
                     <item.icon className="h-5 w-5" />
                     <span className="sr-only">{item.label}</span>
                 </Link>
              </Tab>
           ))}
           <Tab setPosition={setPosition}>
-            <button onClick={toggleTheme} ref={el => linkRefs.current[navItems.length + 1 + actionItems.length] = el ? el.parentElement : null} className="w-full h-full flex items-center justify-center">
+            <button onClick={toggleTheme} ref={el => {
+              if (el) {
+                const parentLi = el.parentElement as HTMLLIElement;
+                linkRefs.current[navItems.length + 1 + actionItems.length] = parentLi;
+              }
+            }} className="w-full h-full flex items-center justify-center">
               <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
               <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
               <span className="sr-only">Toggle theme</span>
