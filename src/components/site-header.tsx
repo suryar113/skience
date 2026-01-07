cbc963b55b90cb084c5d9f6500c11ad0ef124ebf
Surya Raghavan <Surya.Raghvan@gmail.com>
2026-01-07 00:04:50 +0000

    the dark mode icon is off the circle

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -235,9 +235,9 @@ export function SiteHeader() {
               <FileText className="h-[1.2rem] w-[1.2rem]" />
               <span className="sr-only">Document</span>
             </Link>
-            <button onClick={toggleTheme} className="btn-hover-pop p-2 rounded-full border border-input relative">
+            <button onClick={toggleTheme} className="btn-hover-pop p-2 rounded-full border border-input relative flex items-center justify-center">
               <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <Moon className="absolute m-auto h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
               <span className="sr-only">Toggle theme</span>
             </button>
           </div>
