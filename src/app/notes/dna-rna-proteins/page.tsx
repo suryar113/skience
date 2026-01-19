f7ff3880a9022a68bf50437efd7ee546c34e0b6d
Surya Raghavan <Surya.Raghvan@gmail.com>
2026-01-19 02:44:45 +0000

    notes added :D

---
+++ src/app/notes/dna-rna-proteins/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+const EMBED_URL = "https://drinks-hunt-3eb.craft.me/E3FmSQ8z98bsy6";
+
+export default function NotesPage() {
+  return (
+    <div className="w-screen h-screen bg-background">
+      <iframe
+        src={EMBED_URL}
+        className="w-full h-full border-0"
+        allowFullScreen
+      />
+    </div>
+  );
+}
---
+++ src/app/notes/mutations/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+const EMBED_URL = "https://drinks-hunt-3eb.craft.me/GalSPv8qTVLg0t";
+
+export default function NotesPage() {
+  return (
+    <div className="w-screen h-screen bg-background">
+      <iframe
+        src={EMBED_URL}
+        className="w-full h-full border-0"
+        allowFullScreen
+      />
+    </div>
+  );
+}
