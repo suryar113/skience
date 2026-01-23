fff990bea9cfda6db1f4e19ea12f776ad358f49b
Surya Raghavan <Surya.Raghvan@gmail.com>
2026-01-23 23:25:38 +0000

    can you add a little spinner/loading animation between pages?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import NextTopLoader from 'nextjs-toploader';
 
 export const metadata: Metadata = {
   title: "SKIENCE",
@@ -30,6 +31,17 @@ export default function RootLayout({
         <meta name="google-site-verification" content="4P07Vz0gAuX8McY0xvnFrFDUxShRHFNpjCKRfMeJT1g" />
       </head>
       <body className="font-body antialiased bg-background text-foreground" suppressHydrationWarning={true}>
+        <NextTopLoader
+          color="#7a00ff"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3}
+          crawl={true}
+          showSpinner={true}
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #7a00ff,0 0 5px #7a00ff"
+        />
         {children}
         <Toaster />
         <Analytics />
